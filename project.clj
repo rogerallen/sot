b5ea05b8069dff39967b9c39ccec61e8758d012a
Roger Allen <rallen@gmail.com>
2012-06-29 08:30:22 -0700

    add main

--- project.clj
+++ project.clj
@@ -4,5 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [overtone "0.7.1"]])
+                 [overtone "0.7.1"]]
+  :main sot.core)
 
