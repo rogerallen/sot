66530a3ff60662000d8db6f02ec385f3628cc79f
Roger Allen <rallen@gmail.com>
2012-12-22 10:13:11 -0800

    update to RC8

--- src/sot/core.clj
+++ src/sot/core.clj
@@ -3,8 +3,8 @@
   (:gen-class))
 
 (defn -main [& args]
-  (println "testing overtone.  you should hear a 440 Hz sine wave for a few seconds, then the program should stop")
+  (println "testing overtone.  you should hear a 440 Hz sine wave for a few seconds")
   (demo (sin-osc))
   (Thread/sleep 5000)
-  (println "done")
+  (println "done. The program should stop.")
   (System/exit 0))
