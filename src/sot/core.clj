fe5af2b0aac9577ab4e64df37e46adfa3c96c18b
Roger Allen <rallen@gmail.com>
2013-11-23 21:50:51 -0800

    update to test 0.9.0-RC2.  Finally give the demo stereo

--- src/sot/core.clj
+++ src/sot/core.clj
@@ -4,7 +4,7 @@
 
 (defn -main [& args]
   (println "testing overtone.  you should hear a 440 Hz sine wave for a few seconds")
-  (demo (sin-osc))
+  (demo (sin-osc [440 440]))
   (Thread/sleep 5000)
   (println "done. The program should stop.")
   (System/exit 0))
