9ab10a5419a6e9e1d69fe98b182e62b14add1faa
Roger Allen <rallen@gmail.com>
2014-06-11 22:35:35 -0700

    match overtone branch

--- project.clj
+++ project.clj
@@ -9,5 +9,6 @@
   :main ^:skip-aot sot.core
   :target-path "target/%s"
   ;; recent lein new app creation will add this
+  ;; crazy--this needs to be uncommented to get working.  Weird!
   ;; :profiles {:uberjar {:aot :all}}
   :jvm-opts ^:replace [])
