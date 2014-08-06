c8c2c7d13ff5bfb6389a8754b2551a5918ce1212
Roger Allen <rallen@gmail.com>
2014-08-06 06:49:47 -0700

    update clj-native. works with lein 2.4.3

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
 (defproject sot "0.1.0-SNAPSHOT"
-  :description "Simplest overtone test for Issue 108 https://github.com/overtone/overtone/issues/108"
+  :description "Simplest overtone and shadertone test."
   :url "https://github.com/rogerallen/sot"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [overtone "0.9.1"]
-                 [shadertone "0.2.2"] ;; FIXME get to work with 0.2.3
-                 ]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [overtone "0.9.1" :exclusions [[clj-native]]]
+                 [shadertone "0.2.3"]
+                 [clj-native "0.9.5"]]
   :main ^:skip-aot sot.core
   :target-path "target/%s"
   ;; recent lein new app creation will add this
